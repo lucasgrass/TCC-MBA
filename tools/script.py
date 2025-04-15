c6d68e5fa4c0e92191dc531cdc88064c99646afd
lucasgrass <grassberaldo@gmail.com>
2025-04-14 21:41:37 -0300

    Refactor: New structure

--- tools/script.py
+++ tools/script.py
@@ -1,12 +1,14 @@
 import pandas as pd
-
 import os
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
-path_csv_train = "/train.csv"
-path_csv_test = "/test.csv"
+path_csv_train = "../datasets/train.csv"
+path_csv_test = "../datasets/test.csv"
 
-df = pd.read_csv(base_path + path_csv_train)
+train_file_path = os.path.join(base_path, path_csv_train)
+test_file_path = os.path.join(base_path, path_csv_test)
+
+df = pd.read_csv(train_file_path)
 
 df
